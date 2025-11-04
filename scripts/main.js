3eb33a562e3a0319c2910f7171f53f210407290c
John Q. Herman <johnqherman@gmail.com>
2025-11-03 23:30:08 -0500

    add test `exposeGlobals` function

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 console.log("main.js loaded successfully");
 
-alert("hello bro");
+function exposeGlobals(obj) {
+  for (const [key, value] of Object.entries(obj)) {
+    globalThis[key] = value;
+  }
+}
 
 function testFunction() {
   console.log("testFunction was called");
 }
+
+exposeGlobals({
+  testFunction,
+});
