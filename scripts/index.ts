2d23c174610bb7dc1e6ced58be2e18889c220d4b
Liam Stone <trashboyjet@gmail.com>
2025-11-29 22:25:21 -0600

    fullscreen border

--- scripts/index.ts
+++ scripts/index.ts
@@ -273,6 +273,25 @@ runOnStartup(async (runtime: any) => {
   // Styling
   var style = document.createElement("style");
   style.innerText = `
+  /* Border texture */
+	body:before {
+		background:url(border.png);
+		content:"";
+		position:absolute;
+		width:100%;
+		height:100%;
+		opacity:0;
+		animation:fadeBackground 0.5s forwards;
+	}
+
+	/* Border frame */
+	canvas {
+		box-shadow:0px 0px 0px calc(2px * var(--construct-scale)) #000000;
+		background:#000000;
+		opacity:0;
+		animation:fadeBorder 0.25s forwards;
+	}
+
 	/* Input Name styling */
 	.input {
 		background-color:#DDDDDD;
